3de8387095ed648f9c6c29cfdddae6192ab813ed
Ngou-Tin-Kam <ngoutinkam@hotmail.com>
2022-08-18 01:15:19 +0100

    Added ESLint for better formatting and code analysis

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
@@ -6,19 +6,19 @@ const server = app.listen(7300, () => {
   console.log(`Port: ${server.address().port} has started`);
 });
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
 app.get('/getError', (req, res, next) => {
-    next(Error('Error Message'));
+  next(Error('Error Message'));
 });
 
 app.use((err, req, res, next) => {
-    console.log(err.stack);
-    next(err);
+  console.log(err.stack);
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send(err.message);
+  res.status(500).send(err.message);
 });
--- request-handling/index.js
+++ request-handling/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
@@ -6,13 +6,12 @@ const server = app.listen(7300, () => {
   console.log(`Port: ${server.address().port} has started`);
 });
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  const logEntry = 
-    `host: ${req.host}
+  const logEntry = `host: ${req.host}
     ip: ${req.ip}
     method: ${req.method}
     path: ${req.path}
